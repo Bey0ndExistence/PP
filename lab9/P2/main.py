5d0605d812b147c6a77e0c2b2f8a14b73feca3d0
Bey0ndExistence <124349311+Bey0ndExistence@users.noreply.github.com>
2023-05-08 01:04:36 +0300

    Add files via upload

---
+++ lab9/P2/main.py
@@ -0,0 +1,39 @@
+import os
+
+def filter_files(paths):
+
+    for path in paths:
+        if os.path.isfile(path) and path.endswith('.txt'):
+            yield path
+
+def count_lines(paths):
+
+    for path in paths:
+        try:
+            with open(path, 'r') as file:
+                lines = file.readlines()
+                line_count = len(lines)
+                yield (path, line_count)
+        except Exception as e:
+            print(f"Error processing file {path}: {str(e)}")
+
+def display_results(results):
+    """Displays the results"""
+    for path, line_count in results:
+        print(f"{path}: {line_count} lines")
+
+# Exemplu de utilizare
+
+# Lista de path-uri
+file_paths = [
+    '/home/student/Desktop/lab9/P2/file1.txt',
+    '/home/student/Desktop/lab9/P2/file2.txt',
+    '/home/student/Desktop/lab9/P2/file3.jpg',
+    '/home/student/Desktop/lab9/P2/file4.txt'
+]
+
+# Pipeline-ul de generatoare
+numbered_lines = count_lines(filter_files(file_paths))
+
+# Afișarea rezultatelor
+display_results(numbered_lines)
